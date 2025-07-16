64f33c49f172e92610444b7eb040ec7cbfb8c665
eli-10-dev <elijah.gaylan@gmail.com>
2025-07-16 22:02:33 +0800

    Fixed ao12, modified copied ao5 function

--- src/App.tsx
+++ src/App.tsx
@@ -122,9 +122,9 @@ function App() {
 
     newArray = [...array];
     const twelveSoles = newArray.slice(-12);
-    const middleTimes = [...twelveSoles].sort((a: Solve, b: Solve) => a.time - b.time).slice(1, 4);
+    const middleTimes = [...twelveSoles].sort((a: Solve, b: Solve) => a.time - b.time).slice(1, 11);
     const sum = middleTimes.reduce((sum, currentValue) => sum + currentValue.time, 0);
-    const average = (sum / 3).toFixed(3).toString();
+    const average = (sum / 10).toFixed(3).toString();
 
     return average;
   }
